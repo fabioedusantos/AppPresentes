3ebf29a32c10c6ac45db8e789a9641d6bc8d6c60
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2018-11-01 21:11:00 -0300

    fix(api): retorna coleção vazia em consultas sem resultados

--- backend/application/models/PresentesModel.php
+++ backend/application/models/PresentesModel.php
@@ -1,6 +1,6 @@
 <?php
 class PresentesModel extends CI_Model {
-    public function get($id = null) { $query="SELECT * FROM presentes "; if(!empty($id)) $query.="WHERE id = $id "; $query.="ORDER BY dataCadastro DESC"; $query=$this->db->query($query); if(empty($query->result_array())) return null; return count($query->result_array())==1 && !empty($id)?$query->result_array()[0]:$query->result_array(); }
+    public function get($id = null) { $query="SELECT * FROM presentes "; if(!empty($id)) $query.="WHERE id = $id "; $query.="ORDER BY dataCadastro DESC"; $query=$this->db->query($query); if(empty($query->result_array())) return array(); return count($query->result_array())==1 && !empty($id)?$query->result_array()[0]:$query->result_array(); }
     public function insert($data) { return $this->db->insert('presentes',$data); }
     public function update($data,$id) { $this->db->where('id',$id); return $this->db->update('presentes',$data); }
     public function delete($id) { return $this->db->delete('presentes',array('id'=>$id)); }
